e69db7a14aba1e04a4f1cac401033ee8722aeeb8
CharlieHii <charlie_chaper@hotmail.com>
2024-07-10 10:28:27 +0700

    feat: add upload image, update and delete button

--- client/src/pages/user-profile-management/edit-profile-page.jsx
+++ client/src/pages/user-profile-management/edit-profile-page.jsx
@@ -1,9 +1,12 @@
 import EditProfileForm from "../../components/user-profile-management/edit-profile-form.jsx";
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Footer from "../../components/Footer.jsx";
 import NavUser from "./navUser.jsx";
 =======
+=======
+>>>>>>> 557e99d (feat: add upload image, update and delete button)
 import Nav from "../../nav.jsx";
 >>>>>>> 0617cdb (rebase)
 
@@ -27,7 +30,22 @@ function EditProfilePage() {
 >>>>>>> 2b09a65 (rebase)
 =======
 >>>>>>> 6091c76 (rebase)
+<<<<<<< HEAD
 >>>>>>> 0617cdb (rebase)
+=======
+=======
+import Nav from "../../nav.jsx";
+
+function EditProfilePage() {
+  return (
+    <>
+      <Nav />
+      <section className="px-4 py-10 lg:px-60 lg:pt-20 lg:pb-14">
+        <EditProfileForm />
+      </section>
+    </>
+>>>>>>> 2d8e555 (feat: add upload image, update and delete button)
+>>>>>>> 557e99d (feat: add upload image, update and delete button)
   );
 }
 
