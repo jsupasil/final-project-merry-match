f6c839495bf78bfb0df280cf2ffa75c508cd7331
unknown <sornram.spy@gmail.com>
2024-07-11 15:30:58 +0700

    fix:rebase

--- server/app.mjs
+++ server/app.mjs
@@ -1,7 +1,8 @@
 import express from "express";
 import connectionPool from "./src/utils/db.mjs";
 import registerRouter from "../server/src/routes/register.mjs";
-import loginRouter from '../server/src/routes/login.mjs'
+import loginRouter from "../server/src/routes/login.mjs";
+import profileRouter from "../server/src/routes/profile.mjs";
 
 const app = express();
 const port = 4001;
@@ -9,6 +10,7 @@ const port = 4001;
 app.use(express.json());
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+app.use("/profile", profileRouter);
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
