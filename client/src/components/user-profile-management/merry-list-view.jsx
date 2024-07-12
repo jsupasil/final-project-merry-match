c3d97240fbc178782f2333a621a7822cb2991c8b
CharlieHii <charlie_chaper@hotmail.com>
2024-07-13 00:21:46 +0700

    feat: in progress merry-list

--- client/src/components/user-profile-management/merry-list-view.jsx
+++ client/src/components/user-profile-management/merry-list-view.jsx
@@ -1,5 +1,34 @@
 function MerryListView() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <article className="">
+          <h1 className=" text-sm font-semibold text-beige-700 mb-2">
+            PROFILE
+          </h1>
+          <p className=" text-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px] lg:inline-block">
+            Let’s know each other{" "}
+          </p>
+          <p className=" text-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px] lg:inline-block">
+            with Merry!
+          </p>
+        </article>
+        <div>
+          <div>
+            <div>
+              <div></div>
+              <p>Merry to you</p>
+            </div>
+            <div>
+              <div></div>
+              <p>Merry to you</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </>
+  );
 }
 
 export default MerryListView;
--- client/src/pages/user-profile-management/merry-list-page.jsx
+++ client/src/pages/user-profile-management/merry-list-page.jsx
@@ -1,10 +1,14 @@
 import Nav from "../../nav";
 import Footer from "../../components/Footer";
+import MerryListView from "../../components/user-profile-management/merry-list-view";
 
 function MerryListPage() {
   return (
     <>
       <Nav />
+      <section className=" px-[calc((100vw-375px)*254/649)] lg:px-[254px] lg:pt-20 lg:pb-[147px] min-h-[calc(100vh-397px)] lg:min-h-[calc(100vh-459px)]">
+        <MerryListView />
+      </section>
       <Footer />
     </>
   );
