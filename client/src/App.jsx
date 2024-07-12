faaf2bcf98f54a38a8d16a1a03d1c17c19be0381
CharlieHii <charlie_chaper@hotmail.com>
2024-07-12 13:37:28 +0700

    Revert "Merge pull request #9 from jsupasil/feature/non-user/register-page/charlie"
    This reverts commit f332c793822f44f8aabee05202a45fbb980e8ca2, reversing
    changes made to 279b6a3074504442c0ef4a986c37c88577155c23.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,7 @@
 import "./App.css";
-import Nav from "./nav";
-import LandingPage from "./components/landing-page.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import MatchingPage from "./components/MatchingPage";
-import PackageListPage from "./component/packagelist";
-import AddPackagePage from "./component/addpackage";
-import EditPackage from "./component/editpackage";
-import EditProfilePage from "./pages/user-profile-management/edit-profile-page.jsx";
-import RegisterPage from "./pages/non-user/register-page.jsx";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Nav />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/package/add" element={<AddPackagePage />} />
-          <Route path="/package/view" element={<PackageListPage />} />
-          <Route path="/package/edit" element={<EditPackage />} />
-          <Route path="/user/:userId/edit" element={<EditProfilePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* <Route path="/page" element={<Page />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 }
+
 export default App;
